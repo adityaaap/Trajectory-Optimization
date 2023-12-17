473d16dd72b0ae04eb1e0862862346278bd3b06f
adityaaap <aditya.paranjape17@gmail.com>
2023-12-17 17:33:00 -0500

    First

---
+++ MinSnapTraj.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+class MinimumSnap:
+    def __init__(self, globalPath, obstacles, velocity, dt):
+        self.globalPath = globalPath
+        self.obstacles = obstacles
+        self.velocity = velocity
+        self.dt = dt
+        self.n_coeffs = 8
+
+        self.time = []
+        self.A = None
+        self.B = None
+        self.coeffs = None
+        self.noSplines = None
+
+        self.rowCounter = 0
+        self.completeTraj = None
+
+    def getTrajectory(self):
+        self.generateTrajectory()
+        return self.completeTraj
+    
+    def generateTrajectroy(self):
+        self.computeSplineParameters()
+
+    def computeSplineParameters(self):
+        self.createPolyMatrices()
+    
+
+    def createPolyMatrices(self):
+        self.setup()
+        self.positionContraints()
+        self.startGoalConstraints()
+        self.continuityConstraints()
+
+    
+    def setup(self):
+        self.initMatrices()
+        self.numberSplines()
+        self.computeTime()
+        
+    def positionConstraints(self):
+        # waypoint constraints at t=0 and t=T
+
+        # at t=0
+        for i in range(self.noSplines):
+            poly = MinimumSnap.generatepolynomial(self.n_coeffs, order=0, t=0)
+            self.A[self.rowCounter,(self.noSplines - 1)*self.n_coeffs:self.noSplines*self.n_coeffs] = poly
+            self.B[self.rowCounter,:]= self.globalPath[i]
+            self.rowCounter += 1
+
+        # at t=T
+        for i in range(self.noSplines):
+            tT = self.time[i]
+            poly = MinimumSnap.generatepolynomial(self.n_coeffs, order=0, t=tT)
+            self.A[self.rowCounter,(self.noSplines - 1)*self.n_coeffs:self.noSplines*self.n_coeffs] = poly
+            self.B[self.rowCounter,:]= self.globalPath[i+1]
+            self.rowCounter += 1
+
+    def startGoalConstraints(self):
+        for k in [1,2,3]:
+            poly = MinimumSnap.generatepolynomial(self.n_coeffs, k, 0)
+            self.A[self.rowCounter,0:self.n_coeffs] = poly
+            self.rowCounter += 1
+
+        for k in [1,2,3]:
+            poly = MinimumSnap.generatepolynomial(self.n_coeffs, k, t=self.time[-1])
+            self.A[self.rowCounter, (self.noSplines-1) * self.n_coeffs: self.noSplines * self.n_coeffs] = poly
+            self.rowCounter += 1
+
+    
+    def continuityConstraints(self):
+
+    def initMatrices(self):
+        self.A = np.zeros((self.n_coeffs * self.noSplines, self.n_coeffs * self.noSplines))
+        self.B = np.zeros((self.n_coeffs * self.noSplines, self.globalPath.shape[1])) # or len(self.globalPath[0])
+
+    def numberSplines(self):
+        self.noSplines = self.globalPath.shape[0] - 1
+
+
+    def computeTime(self):
+        vel = self.velocity
+        for i in range(self.noSplines):
+            dist = np.linalg.norm(self.globalPath[i+1] - self.globalPath[i])
+            self.time.append(dist/vel)
+
+
+
+
+    @staticmethod
+    def generatepolynomial(noCoeffs, order, t):
+        poly = np.zeros(noCoeffs)
+        deri = np.zeros(noCoeffs)
+
+        for i in range(noCoeffs):
+            poly[i] = i
+            deri[i] = 1
+        
+        for i in range(order):
+            for j in range(noCoeffs):
+                poly[i] = poly[i] * deri[i]
+                if(deri[i] > 0):
+                    deri[i] = deri[i] - 1
+        
+        for i in range(noCoeffs):
+            poly[i] = poly[i] * t**deri[i]
+
+        return poly.T
+
+    ## More static methods may be required for checking collision
---
+++ main.py
@@ -0,0 +1,56 @@
+from matplotlib import pyplot as plt
+# from RRTStar_FromUAV import RRTStar
+from RRTStar_forTrajOptim import RRTStar
+from MinSnapTraj_FromUAV import MinimumSnap
+import numpy as np
+
+start = np.array([0, 0, 0])
+goal = np.array([7.0*10, 7.0*10, 7.0*10]) # Dont keep goal as integer values
+
+space_limits = np.array([[0., 0., 0.9], [100., 100., 100.]])
+
+rrt = RRTStar(
+    space_limits,
+    start=start,
+    goal=goal,
+    max_distance=5,
+    max_iterations=1000,
+    obstacles=None,
+)
+
+# rrt = RRTStar(space_limits, start, goal, max_distance, max_iterations, obstacles)
+rrt.run()
+global_path = rrt.bestPath  # long-term path
+print(global_path)
+
+min_snap = MinimumSnap(global_path, obstacles=None, velocity=2, dt=0.1)
+global_trajectory = min_snap.get_trajectory()  # long-term trajectory
+positions = min_snap.positions
+# print(positions)
+
+# rrt.plot()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# tree = list(self.bestPath.values())  # Extract values from the dictionary
+# tree = self.bestPath
+tree = np.array(positions) 
+
+# print(tree)
+# Extracting x, y, z coordinates from the array
+x = tree[:, 0]
+y = tree[:, 1]
+z = tree[:, 2]
+
+# Plotting the points
+# ax.scatter(x, y, z)
+for i in range(len(tree) - 1):
+    ax.plot([x[i], x[i + 1]], [y[i], y[i + 1]], [z[i], z[i + 1]], color='blue')
+
+# Set labels
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+
+plt.show()
+# print(global_trajectory)
